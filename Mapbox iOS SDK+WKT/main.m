16378e938fcf855face1c6d6dc07c4e7090b3769
awedeebawe <lyubomirm@eatglobe.com>
2014-09-09 18:36:56 +0300

    v1.0
    This reverts commit 6b4881d957bf63d3371abd15f8211d2ff8e408d8.

---
+++ Mapbox iOS SDK+WKT/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mapbox iOS SDK+WKT
+//
+//  Created by user on 9/8/14.
+//  Copyright (c) 2014 marinov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ADBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ADBAppDelegate class]));
+    }
+}
